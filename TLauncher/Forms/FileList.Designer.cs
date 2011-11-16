df77a7814b8fc7cf95c3a11e35ffee1fbd660cd1
DeathCradle <DeathCradle@live.com.au>
2011-11-17 08:15:10 +1000

    Added new files...

---
+++ TLauncher/Forms/FileList.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TLauncher.Forms
+{
+    partial class FileList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lv_Files = new System.Windows.Forms.ListView();
+            this.Clm_File = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Clm_Extension = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Clm_Path = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // Lv_Files
+            // 
+            this.Lv_Files.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Clm_File,
+            this.Clm_Extension,
+            this.Clm_Path});
+            this.Lv_Files.FullRowSelect = true;
+            this.Lv_Files.GridLines = true;
+            this.Lv_Files.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.Lv_Files.HoverSelection = true;
+            this.Lv_Files.Location = new System.Drawing.Point(12, 12);
+            this.Lv_Files.Name = "Lv_Files";
+            this.Lv_Files.Size = new System.Drawing.Size(401, 256);
+            this.Lv_Files.TabIndex = 0;
+            this.Lv_Files.UseCompatibleStateImageBehavior = false;
+            this.Lv_Files.View = System.Windows.Forms.View.Details;
+            this.Lv_Files.DoubleClick += new System.EventHandler(this.Lv_Files_DoubleClick);
+            // 
+            // Clm_File
+            // 
+            this.Clm_File.Text = "File Name";
+            this.Clm_File.Width = 334;
+            // 
+            // Clm_Extension
+            // 
+            this.Clm_Extension.Text = "Ext.";
+            // 
+            // Clm_Path
+            // 
+            this.Clm_Path.Text = "";
+            this.Clm_Path.Width = 0;
+            // 
+            // FileList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 280);
+            this.Controls.Add(this.Lv_Files);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FileList";
+            this.ShowInTaskbar = false;
+            this.Text = "FileList";
+            this.Load += new System.EventHandler(this.FileList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView Lv_Files;
+        private System.Windows.Forms.ColumnHeader Clm_File;
+        private System.Windows.Forms.ColumnHeader Clm_Extension;
+        private System.Windows.Forms.ColumnHeader Clm_Path;
+    }
+}
---
+++ TLauncher/Forms/FileList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace TLauncher.Forms
+{
+    public partial class FileList : Form
+    {
+        public FileList()
+        {
+            InitializeComponent();
+        }
+
+        private void FileList_Load(object sender, EventArgs e)
+        {
+            Lv_Files.Items.Clear();
+
+            string Path = Directory.GetParent(Process.GetCurrentProcess().MainModule.FileName).FullName;
+            foreach (string file in Directory.GetFiles(Path))
+            {
+                Lv_Files.Items.Add(
+                    GetListViewItem(file)
+                );
+            }
+        }
+
+        public ListViewItem GetListViewItem(string File)
+        {
+            string file = (new FileInfo(File)).Name;
+            string name =  file.Substring(0, file.LastIndexOf("."));
+            string ext = file.Remove(0, name.Length + 1).ToUpper();
+            ListViewItem item = new ListViewItem()
+            {
+                Text = name
+            };
+            foreach (string sItem in new string[] { ext, File })
+            {
+                item.SubItems.Add(new ListViewItem.ListViewSubItem()
+                {
+                    Text = sItem
+                });
+            }
+            return item;
+        }
+
+        private void Lv_Files_DoubleClick(object sender, EventArgs e)
+        {
+            ListView lv = sender as ListView;
+            ListViewItem item = lv.SelectedItems[0];
+            string path = item.SubItems[2].Text;
+            Process.Start(path);
+        }
+    }
+}
